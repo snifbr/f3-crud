0a7f0d310c864af8d1cf380d3890087e2c7199d5
Danilo Figueiredo Rocha <danilo@ic.unicamp.br>
2019-06-04 17:30:09 -0300

    Changed fatfree lib to autoload.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$f3=require('lib/base.php');
+#$f3=require('lib/base.php');
+require 'vendor/autoload.php';
+$f3 = \Base::instance();
 $f3->config('config/config.ini');
 $f3->config('config/routes.ini');
 $f3->run();
