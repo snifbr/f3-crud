1d7c1ea5960ed02c076bf6949d89397be1aa206e
Danilo Figueiredo Rocha <snifbr@gmail.com>
2019-06-20 23:02:41 -0300

    update phpunit Class name.

--- tests/PHPNativeElementsTest.php
+++ tests/PHPNativeElementsTest.php
@@ -1,37 +1,37 @@
-<?php 
+<?php
 
-use PHPUnit_Framework_TestCase as PHPUnit;
+use PHPUnit\Framework\TestCase as PHPUnit;
 use Application\NativeElements\Math;
 
-class PHPNativeElementsTest extends PHPUnit 
+class PHPNativeElementsTest extends PHPUnit
 {
 
     protected $math;
-  
+
     public function setUp()
     {
 	$this->math = new Math;
     }
-    
-    
+
+
     public function testOperacaoMatematica()
     {
 	$this->math->setOperator('plus');
 	$this->assertEquals(3, $this->math->getResult(1, 2), 'Não somou corretamente');
-	
+
 	$this->math->setOperator('minus');
 	$this->assertEquals(1, $this->math->getResult(2, 1), 'Não subtratiu corretamente');
-	
+
 	$this->math->setOperator('obleous');
 	$this->assertEquals(3, $this->math->getResult(9, 3), 'Não dividiu corretamente');
-	
+
 	$this->math->setOperator('times');
 	$this->assertEquals(4, $this->math->getResult(2, 2), 'Não multiplicou corretamente');
     }
-    
+
     public function tearDown()
     {
-      
+
     }
-  
+
 }
